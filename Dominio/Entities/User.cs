d99c281d77c43b1571b683d46c94a75764d64473
Razenth <santiago.gonzalezagudelo@gmail.com>
2023-10-19 13:10:07 -0500

    feat: :sparkles: Implementando nuevas cosas
    En primer lugar se hizo la conexion a la base de datos, configuracion de Cors y JWT por medio de ApplicationServicesExtensions, se creo un Helper llamado JWT  que tiene las propiedades default del JSON WEB TOKEN, en el program se enlazo la base de datos, se creó unas entidades que luego se enlazaron en los Services. Finalmente en el JSON se implemento el token JWT

---
+++ Dominio/Entities/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Entities
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Rol> Rols { get; set; }
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new HashSet<RefreshToken>();
+        public ICollection<UserRol> UserRols { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistencia.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Proyecto4CapasContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
